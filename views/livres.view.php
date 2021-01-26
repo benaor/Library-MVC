dd6c16191747246a05635ebd9cbb96f6ef601033
benaor <benjamingirard25@gmail.com>
2021-01-26 22:44:45 +0100

    Add link to showBook

--- views/livres.view.php
+++ views/livres.view.php
@@ -14,7 +14,9 @@
     ?>
         <tr>
             <td class="align-middle"><img src="../public/images/<?= $book->getImage() ?>" width="60px;" /></td>
-            <td class="align-middle"><?= $book->getTitle() ?></td>
+            <td class="align-middle">
+            <a href="<?= URL ?>livres/show/<?= $book->getId() ?>"><?= $book->getTitle() ?></a>
+            </td>
             <td class="align-middle"><?= $book->getnbPages() ?></td>
             <td class="align-middle"> <a href="" class="btn btn-warning"> Modifier</a> </td>
             <td class="align-middle"> <a href="" class="btn btn-danger"> Supprimer</a> </td>
